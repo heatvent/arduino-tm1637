80f63564b7c05f3d90aa57e19ca5c6a71c2cc40d
heatvent <matteocp@gmail.com>
2018-10-16 11:59:51 -0500

    Update SevenSegmentTM1637.h

--- src/SevenSegmentTM1637.h
+++ src/SevenSegmentTM1637.h
@@ -192,8 +192,14 @@ public:
     /* Turn the decimal on or off
   * When turning the decimal on, the next displayed text/numbers will have a decimal
   @param [in] setToOn       sets the decimal to on or off
+  @param [in] position      sets the position/character to write to; if not identified when turning on decimals, defaults to 1 / first character printed
   */
-  void    setDecimalOn(bool setToOn, uint8_t position = 0);
+  void    setDecimalOn(bool setToOn, uint8_t position = 1);
+     /* Set the decimal position only
+  * Decimal position is in relation to the position being printed.  So if you select 2 for example, the decimal will be added to the second character printed, regardless of it's actual position on the LCD
+  @param [in] position      sets/changes the position/character to write to, defautls to 1 / first character printed
+  */
+  void    setDecimalPosition(uint8_t position = 1);
   /* Get the current decimal setting
   */
   bool    getDecimalOn(void);
