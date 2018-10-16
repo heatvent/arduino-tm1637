9e510f913f5a19299075614babaaf383f0d852de
heatvent <matteocp@gmail.com>
2018-10-16 12:01:58 -0500

    Update SevenSegmentTM1637.cpp

--- src/SevenSegmentTM1637.cpp
+++ src/SevenSegmentTM1637.cpp
@@ -293,8 +293,14 @@ bool  SevenSegmentTM1637::getDecimalOn(void) {
 };
 
 void  SevenSegmentTM1637::setDecimalOn(bool setToOn, uint8_t position) {
-    if (position <=3) {
+    if (position <=4) {
       _decimalOn = setToOn;
+      _decimalPosition = position;
+    }
+}
+
+void  SevenSegmentTM1637::setDecimalPosition(uint8_t position) {
+    if (position <=4) {
       _decimalPosition = position;
     }
 }
