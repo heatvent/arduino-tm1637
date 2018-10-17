a3df7e7cbd81a0644270cb8129a3744dfefde9a2
heatvent <matteocp@gmail.com>
2018-10-16 20:43:10 -0500

    Update SevenSegmentDecimal.cpp

--- src/SevenSegmentDecimal.cpp
+++ src/SevenSegmentDecimal.cpp
@@ -1,18 +1,18 @@
 
 #include "SevenSegmentDecimal.h"
 
-bool  SevenSegmentTM1637::getDecimalOn(void) {
+bool  SevenSegmentDecimal::getDecimalOn(void) {
   return (_decimalOn);
 };
 
-void  SevenSegmentTM1637::setDecimalOn(bool setToOn, uint8_t position) {
+void  SevenSegmentDecimal::setDecimalOn(bool setToOn, uint8_t position) {
     if (position <=4) {
       _decimalOn = setToOn;
       _decimalPosition = position;
     }
 }
 
-void  SevenSegmentTM1637::setDecimalPosition(uint8_t position) {
+void  SevenSegmentDecimal::setDecimalPosition(uint8_t position) {
     if (position <=4) {
       _decimalPosition = position;
     }
